344675341221130f4bdc001ed46fecaa5cc1beb6
Aine <ainekilbane@gmail.com>
2025-08-27 11:28:22 +0100

    forgot to call the function in the first place

--- src/scripts.js
+++ src/scripts.js
@@ -1,6 +1,7 @@
 function give_feedback() {
     //print the user's query back to the page for verification
     //can call the http request from another function in here later etc
+    send_request()
     const userInput = document.getElementById("user_input").value;
     //console.log(userInput)
     const feedback_element = document.getElementById("feedback_element");
