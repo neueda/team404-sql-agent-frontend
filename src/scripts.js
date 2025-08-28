3280171dcd271295ea04b0df969aade2ea14f9c7
Maciej <maciejwlod@outlook.com>
2025-08-28 12:38:20 +0100

    HTML Table populated with JSON Data

--- src/scripts.js
+++ src/scripts.js
@@ -1,18 +1,18 @@
 function give_feedback() {
-    //print the user's query back to the page for verification
-    const userInput = document.getElementById("user_input").value;
-    //console.log(userInput)
-    const feedback_element = document.getElementById("feedback_element");
-    if (userInput) {
-      send_request(userInput)
-      feedback_element.innerText = userInput;
-    }
-    else {
-      feedback_element.innerText = "No query entered"
-    }
+  //print the user's query back to the page for verification
+  const userInput = document.getElementById("user_input").value;
+  //console.log(userInput)
+  const feedback_element = document.getElementById("feedback_element");
+  if (userInput) {
+    send_request(userInput)
+    feedback_element.innerText = userInput;
+  }
+  else {
+    feedback_element.innerText = "No query entered"
+  }
 }
 
-async function send_request(user_input) {  
+async function send_request(user_input) {
   const url = `http://localhost:8080/api/hello?query=${user_input}`; //change to actual URL when we have GC access
   try {
     const response = await fetch(url);
@@ -21,17 +21,23 @@ async function send_request(user_input) {
     }
 
     const results = await response.json();
-    console.log(results); //for testing and troubleshooting purposes
+
+    use_response(results);
 
   } catch (error) {
     console.error(error.message);
   }
-  use_response(results);
 }
 
 function use_response(result_json) {
-    //placeholder - will fill the dynamic result table with data
-    const table = document.getElementById("results");
-    //assign values to table.innerHTML
-    //one row per row in JSON
+  //Populates the HTML Table element with JSON object.
+  const table = document.getElementById("results");
+
+  // Loop through all keys in the result_json object
+
+  for (let key in result_json) {
+    if (result_json.hasOwnProperty(key)) {
+      table.innerHTML += `${key} : ${result_json[key]}<br>`;
+    }
+  }
 }
