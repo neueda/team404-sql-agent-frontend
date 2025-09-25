c517f2486fc3e5888376dc9f3f6ac36e5e594f05
SiÃÃnéad <sinead.madden@zinkworks.com>
2025-09-25 16:08:41 +0100

    added page + table to index

--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,11 @@
+// listens for enter when user submits input
+document.querySelector('#user_input').addEventListener('keydown', function (e){
+  if (e.key === 'Enter') {
+    give_feedback()
+  }
+})
+
+
 function give_feedback() {
   //print the user's query back to the page for verification
   const userInput = document.getElementById("user_input").value;
@@ -13,7 +21,7 @@ function give_feedback() {
 }
 
 async function send_request(user_input) {
-  const url = `https://team404-sql-agent-971987703066.europe-north1.run.app/api/hello?query=${user_input}`;
+  const url = `http://localhost:8080/api/hello?query=${user_input}`;
   try {
     const response = await fetch(url);
     if (!response.ok) {
@@ -31,13 +39,30 @@ async function send_request(user_input) {
 
 function use_response(result_json) {
   //Populates the HTML Table element with JSON object.
-  const table = document.getElementById("results");
+  const headerRow = document.getElementById("Hresults");
+  const tableBody = document.getElementById("results");
+  const headers = ["Film", "Genre", "Lead Studio", "Audience Score", "Profitability", "Rotten Tomatoes Score", "Worldwide Gross Revenue", "Year"];
+  //var table = document.getElementById("table");
+
+
+  var headhtml = "<tr>";
+  for (h in headers){
+    headhtml += `<th>${headers[h]}</th>`
+  }
+  headhtml += "</tr>";
+  headerRow.innerHTML = headhtml
 
   // Loop through all keys in the result_json object
-
-  for (let key in result_json) {
-    if (result_json.hasOwnProperty(key)) {
-      table.innerHTML += `${key} : ${result_json[key]}<br>`;
+  var bodyHTML = "";
+  for(row in result_json) {
+    bodyHTML += "<tr>";
+    for (col in result_json[row]){
+      bodyHTML += "<td>";
+      bodyHTML += `${result_json[row][col]}`;
+      bodyHTML +=  "<td>";
     }
+    bodyHTML += "</tr>";
   }
+  tableBody.innerHTML = bodyHTML;
+
 }
